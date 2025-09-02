3a3ea161981325dd76114b9faa9f58b908cc2bcd
uriel.martinez@conjur.mx <uriel.martinez@conjur.mx>
2025-09-02 16:09:26 -0600

    ajuste ramificacion

--- src/hu/dashboard/CoorinDashboard.jsx
+++ src/hu/dashboard/CoorinDashboard.jsx
@@ -9,7 +9,6 @@ import RamificacionSesiones from "./board/consultations/RamificacionSesiones";
 export default function CoorinDashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedExecutiveId, setSelectedExecutiveId] = useState(null);
-
   const handleSidebarToggle = () => setSidebarOpen((prev) => !prev);
 
   return (
@@ -39,7 +38,6 @@ export default function CoorinDashboard() {
               <GridExecutives />
             </div>
           </div>
-
           <div className="col-span-6 row-start-2 relative">
             <div className="flex justify-between items-start mb-4 mt-4">
               <span className="text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-4xl">
@@ -50,37 +48,13 @@ export default function CoorinDashboard() {
               <GridConsultations />
             </div>
           </div>
-
           <div className="grid grid-cols-2 gap-4 mt-8 w-full">
             <div className="relative w-full">
               <RamificacionSesiones onExecutiveSelect={setSelectedExecutiveId} />
             </div>
-            <div className="relative bg-white shadow-lg ring-1 ring-black/5 rounded-2xl flex flex-col p-6 w-full h-82">
-              <h3 className="text-lg font-semibold mb-4 flex items-center text-gray-800">
-                <span className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="inline-block w-6 h-6 text-gray-700"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M7 7a3 3 0 11-6 0 3 3 0 016 0zm0 0v10a3 3 0 006 0V7m0 10a3 3 0 006 0V7a3 3 0 10-6 0"
-                    />
-                  </svg>
-                </span>
-                Ramificación
-              </h3>
-              <div className="bg-[#e5ebf2] rounded-lg flex-1 min-h-[120px]"></div>
-            {/* Tabla de Sesiones */}
-            <div className="relative w-full">
+             <div className="relative w-full">
               <TablaSesiones selectedExecutiveId={selectedExecutiveId} />
             </div>
-          </div>
         </div>
       </div>
     </div>
