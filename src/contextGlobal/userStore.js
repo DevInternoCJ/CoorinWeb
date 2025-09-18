8da9de7501a335cace3c40a616794837e586f964
UrielMartinezCj <uriel.martinez@conjur.mx>
2025-09-18 12:11:49 -0600

    merge de cambios global

--- src/contextGlobal/userStore.js
+++ src/contextGlobal/userStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 
+//estato global del usuario de inicio de sesion(store )
 export const useUserStore = create((set) => ({
   user: null,
   setUser: (user) => set({ user }),
