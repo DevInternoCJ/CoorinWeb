41dc56cdfa58493bf9b27f0967f3e9e747d76576
YoshuaRodriguez <yoshua.rodriguez@conjur.mx>
2025-10-03 19:25:35 -0600

    Ajuste de resetear-contraseña y nuevo endpoint asignar-encargado-cartera

--- Mark/Administracion/Ejecutivos/Encargados/Interfaces/IEncargadosService.cs
+++ Mark/Administracion/Ejecutivos/Encargados/Interfaces/IEncargadosService.cs
@@ -1,4 +1,5 @@
-﻿using Loki.DTOs.SesionesDTOs;
+﻿using Loki.DTOs.EncargadosDTOs;
+using Loki.DTOs.SesionesDTOs;
 
 namespace Loki.Mark.Administracion.Ejecutivos.Encargados.Interfaces
 {
@@ -6,5 +7,7 @@ namespace Loki.Mark.Administracion.Ejecutivos.Encargados.Interfaces
     {
         Task<List<object>> GetAllEncargados(string servidor);
         Task<List<EjecutivoConJerarquia>> JerarquiaEjecutivosPropios(string servidor, int idEjecutivo);
-    }
+		Task<List<ResultadoAsignacionDto>> CambiarEncargado(string servidor, List<CambiaEncargadoDto> cambiaEncargado);
+
+	}
 }
