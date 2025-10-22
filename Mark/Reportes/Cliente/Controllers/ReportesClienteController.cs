09addfd2f42d2623de7d4b1972f49d03402b6796
YoshuaRodriguez <yoshua.rodriguez@conjur.mx>
2025-10-21 19:30:47 -0600

    Remoción del Anonymous para los endpoints de Reportes al Ciente

--- Mark/Reportes/Cliente/Controllers/ReportesClienteController.cs
+++ Mark/Reportes/Cliente/Controllers/ReportesClienteController.cs
@@ -25,11 +25,10 @@ namespace Loki.Mark.Reportes.Cliente.Controllers
 			Summary = "Obtener Definiciones de Reportes - Yoshi",
 			Description = "Devuelve la lista de reportes disponibles y los parámetros que requiere cada uno.")]
 		[ProducesResponseType(typeof(IEnumerable<ReporteDefinicionDto>), 200)]
-		[AllowAnonymous]
 		public async Task<IActionResult> GetDefiniciones()
 		{
-			//string? servidorClaim = User.FindFirst("Servidor")?.Value;
-			string? servidorClaim = "Albaz";
+			string? servidorClaim = User.FindFirst("Servidor")?.Value;
+			//string? servidorClaim = "Albaz";
 
 			if (string.IsNullOrWhiteSpace(servidorClaim))
 				return Unauthorized(new { error = "Claim 'Servidor' no encontrado." });
@@ -45,11 +44,10 @@ namespace Loki.Mark.Reportes.Cliente.Controllers
 		[ProducesResponseType(typeof(Dictionary<string, IEnumerable<dynamic>>), 200)]
 		[ProducesResponseType(typeof(object), 404)]
 		[ProducesResponseType(typeof(object), 400)]
-		[AllowAnonymous]
 		public async Task<IActionResult> GenerarReporte([FromBody] GenerarReporteRequestDto request)
 		{
-			//string? servidorClaim = User.FindFirst("Servidor")?.Value;
-			string? servidorClaim = "Albaz";
+			string? servidorClaim = User.FindFirst("Servidor")?.Value;
+			//string? servidorClaim = "Albaz";
 
 			if (string.IsNullOrWhiteSpace(servidorClaim))
 				return Unauthorized(new { error = "Claim 'Servidor' no encontrado." });
